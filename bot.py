6bf40ba02d560e40eac96b0799c9b2cee0132aa9
sharint <tim.kulizhnikov@mail.ru>
2023-09-30 19:04:59 +0700

    refactor bot answer

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from telebot import types
 import telebot
 import dbService
 import uuid
@@ -8,9 +9,22 @@ token = "6339026680:AAHFh25ZDH2bxLvuNIvH7bQLYI6Y6MpPjsU"
 bot = telebot.TeleBot(token, parse_mode=None)
 language='ru_RU'
 
+GLOSSARIY = "Глоссарий"
+AI = "Исскуственный интелект"
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Привет! Я бот-помощник sizifs. Я помогаю")
+	markup = types.ReplyKeyboardMarkup()
+	btn = types.KeyboardButton(GLOSSARIY)
+	markup.add(btn)
+	bot.reply_to(message, "Привет! Я бот-помощник sizif. Чем могу помочь?",reply_markup=markup)
+     
+@bot.message_handler(content_types=['text'])
+def func(message):
+	if(message.text == GLOSSARIY):
+		switchKeyboardbutton(message,AI)
+	if(message.text == AI):
+		switchKeyboardbutton(message,GLOSSARIY)
 	
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
@@ -47,5 +61,12 @@ def voice_processing(message):
     bot.reply_to(message, text)
     os.remove(file_name_full)
     os.remove(file_name_full_converted)
+
+def switchKeyboardbutton(message,buttonText):
+	markup = types.ReplyKeyboardMarkup()
+	btn = types.KeyboardButton(buttonText)
+	markup.add(btn)
+	text = "Теперь я беру данные из "+ buttonText
+	bot.send_message(message.chat.id,text=text,reply_markup=markup)
 
 bot.infinity_polling()
